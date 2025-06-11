f73b78ec5bcff15aeb7b65e8baeda5dab40a38b2
Zadvenul <twetwetwe12e@gmail.com>
2025-06-11 21:41:16 +0500

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,1 @@
-const newWindow = window.open("https://www.roblox.com/game-pass/1255669814/Gamepass", "_blank")
-
-newWindow.onload = function() {
-    newWindow.document.getElementsByClassName('action-button')[0].querySelector('button').click()
-}
+window.open("https://www.roblox.com/game-pass/1255669814/Gamepass", "_blank")
